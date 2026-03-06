538b3e9b1350cecb707481b0a3b6df4375b50c94
AQian <61798525+AQian0@users.noreply.github.com>
2026-03-06 16:25:55 +0800

    feat(frontend): 改进后端复用 (#17)
    * feat(frontend): 引入@elysiajs/eden
    
    * build(frontend): 添加后端项目路径别名

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "#backend": fileURLToPath(new URL("../backend/src", import.meta.url)),
     },
   },
 });
